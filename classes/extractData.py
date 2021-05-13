c2418b4abab810dacdf9e61c4b789367f691baed
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-05-13 16:46:47 +0430

    Handle the ElementNotFound exception

--- classes/extractData.py
+++ classes/extractData.py
@@ -1,10 +1,9 @@
+import selenium.common.exceptions
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 from unidecode import unidecode
 import json
-import string
-
+from colorama import Fore
 
 class ExtractData:
     # Initial the class
@@ -22,9 +21,9 @@ class ExtractData:
         # Open the JSON file
         with open('links.json', 'r', encoding='utf8') as f:
             # Load the JSON file
-            jsonLinks = json.load(f)
+            json_links = json.load(f)
             # Extract the links from the JSON file
-            for link in jsonLinks:
+            for link in json_links:
                 self.__links.append(link)
 
     # Extract the phone numbers and append them to the list
@@ -36,87 +35,102 @@ class ExtractData:
             if self.__counter == 0:
                 # Load the website
                 self.__driver.get(link)
-                # Click on the 'Get Contact' button
-                self.__driver.find_element_by_css_selector(
-                    'button.post-actions__get-contact').click()
-                # Click on the 'Accept the rules button
-                self.__driver.find_element_by_css_selector(
-                    'footer.kt-modal__actions  button.kt-button--primary').click()
-                # Request the phone number from the user
-                user_phone_number = input("Please enter your phone number: ")
-                # Send the phone number to the input
-                self.__driver.find_element_by_css_selector(
-                    'div.kt-textfield input').send_keys(user_phone_number)
-                # Request the phone number verification code from the user
-                user_verify_code = input(
-                    'Please enter the verification code: ').strip()
-                # Send the phone number verification code to the input
-                self.__driver.find_element_by_css_selector(
-                    'article.auth-modal div.kt-modal__contents input').send_keys(user_verify_code)
-                # Wait 2 seconds
-                time.sleep(2)
-                # Grab the phone number box
-                phone_number_box = self.__driver.find_element_by_css_selector(
-                    'div.expandable-box')
-                # Extract the phone number from the Element
-                phone_number = phone_number_box.find_element_by_tag_name(
-                    'a').text
+                try:
+                    # Click on the 'Get Contact' button
+                    self.__driver.find_element_by_css_selector(
+                        'button.post-actions__get-contact').click()
+                    # Click on the 'Accept the rules button
+                    self.__driver.find_element_by_css_selector(
+                        'footer.kt-modal__actions  button.kt-button--primary').click()
+                    # Request the phone number from the user
+                    user_phone_number = input("Please enter your phone number: ")
+                    # Send the phone number to the input
+                    self.__driver.find_element_by_css_selector(
+                        'div.kt-textfield input').send_keys(user_phone_number)
+                    # Request the phone number verification code from the user
+                    user_verify_code = input(
+                        'Please enter the verification code: ').strip()
+                    # Send the phone number verification code to the input
+                    self.__driver.find_element_by_css_selector(
+                        'article.auth-modal div.kt-modal__contents input').send_keys(user_verify_code)
+                    # Wait 2 seconds
+                    time.sleep(2)
+                    # Grab the phone number box
+                    phone_number_box = self.__driver.find_element_by_css_selector(
+                        'div.expandable-box')
+                    # Extract the phone number from the Element
+                    phone_number = phone_number_box.find_element_by_tag_name(
+                        'a').text
 
-                # Convert the phone numbers to English digits
-                phone_number = unidecode(phone_number)
-                # Select the title
-                post_title = self.__driver.find_element_by_tag_name('h1').text
-                # select discription
-                discription = self.__driver.find_element_by_css_selector(
-                    'p.kt-description-row__text').text
-                # Select the tag Box
-                tags = list()
-                tagsBox = self.__driver.find_element_by_css_selector(
-                    'div.kt-wrapper-row')
-                for tag in tagsBox.find_elements_by_tag_name('a'):
-                    tags.append(tag.text)
-                # Append data to list
-                self.__data_list.append({
-                    'postTitle': post_title,
-                    'posterDiscription': discription,
-                    'posterPhone': phone_number,
-                    'postTags': tags
-                })
-                # Increase the counter by one
-                self.__counter += 1
+                    # Convert the phone numbers to English digits
+                    phone_number = unidecode(phone_number)
+                    # Select the title
+                    post_title = self.__driver.find_element_by_tag_name('h1').text
+                    # select discription
+                    discription = self.__driver.find_element_by_css_selector(
+                        'p.kt-description-row__text').text
+                    # Select the tag Box
+                    tags = list()
+                    tags_box = self.__driver.find_element_by_css_selector(
+                        'div.kt-wrapper-row')
+                    for tag in tags_box.find_elements_by_tag_name('a'):
+                        tags.append(tag.text)
+                    # Append data to list
+                    self.__data_list.append({
+                        'postTitle': post_title,
+                        'posterDiscription': discription,
+                        'posterPhone': phone_number,
+                        'postTags': tags
+                    })
+                    # Increase the counter by one
+                    self.__counter += 1
+                except selenium.common.exceptions.NoSuchElementException:
+                    print(f'{Fore.RED}ELement does not exists{Fore.WHITE}')
             else:
-                # Load the website
-                self.__driver.get(link)
-                # Click on the 'Get Contact' button
-                self.__driver.find_element_by_css_selector(
-                    'button.post-actions__get-contact').click()
-                # Wait 2 seconds
-                time.sleep(2)
-                # Grab the phone number box
-                phone_number_box = self.__driver.find_element_by_css_selector(
-                    'div.expandable-box')
-                # Extract the phone number from the Element
-                phone_number = phone_number_box.find_element_by_tag_name(
-                    'a').text
-                # Convert the phone numbers to English digits
-                phone_number = unidecode(phone_number)
-                # Select the title
-                post_title = self.__driver.find_element_by_tag_name('h1').text
-                # select discription
-                discription = self.__driver.find_element_by_css_selector(
-                    'p.kt-description-row__text').text
-                # Select the tag Box
-                tags = list()
-                tagsBox = self.__driver.find_element_by_css_selector(
-                    'div.kt-wrapper-row')
-                for tag in tagsBox.find_elements_by_tag_name('a'):
-                    tags.append(tag.text)
-                # Append data to list
-                self.__data_list.append({
-                    'postTitle': post_title,
-                    'posterDiscription': discription,
-                    'posterPhone': phone_number,
-                    'postTags': tags
-                })
+                try:
+                    # Load the website
+                    self.__driver.get(link)
+                    # Click on the 'Get Contact' button
+                    self.__driver.find_element_by_css_selector(
+                        'button.post-actions__get-contact').click()
+                    # Wait 2 seconds
+                    time.sleep(2)
+                    # Grab the phone number box
+                    phone_number_box = self.__driver.find_element_by_css_selector(
+                        'div.expandable-box')
+                    # Extract the phone number from the Element
+                    phone_number = phone_number_box.find_element_by_tag_name(
+                        'a').text
+                    # Convert the phone numbers to English digits
+                    phone_number = unidecode(phone_number)
+                    # Select the title
+                    post_title = self.__driver.find_element_by_tag_name('h1').text
+                    # select discription
+                    discription = self.__driver.find_element_by_css_selector(
+                        'p.kt-description-row__text').text
+                    # Select the tag Box
+                    tags = list()
+                    tags_box = self.__driver.find_element_by_css_selector(
+                        'div.kt-wrapper-row')
+                    for tag in tags_box.find_elements_by_tag_name('a'):
+                        tags.append(tag.text)
+                    # Append data to list
+                    self.__data_list.append({
+                        'postTitle': post_title,
+                        'posterDiscription': discription,
+                        'posterPhone': phone_number,
+                        'postTags': tags
+                    })
+                except selenium.common.exceptions.NoSuchElementException:
+                    print(f'{Fore.RED}ELement does not exists{Fore.WHITE}')
         self.__driver.quit()
-        print(self.__data_list)
+        print('Information saved in data.json')
+        self.save_data()
+
+    # Save the data in the json file
+    def save_data(self):
+        # Open the json file
+        with open('data.json', 'w+', encoding='utf8') as f:
+            # Append the json to file
+            json.dump(self.__data_list, f, ensure_ascii=False)
+
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     # Create instance of GetPosterLink
     get_poster_links = GetPosterLink(timeout=5)
     # Scrap the links
-    get_poster_links.scrapData()
+    get_poster_links.scrap_data()
     # Save the scraped links
-    get_poster_links.saveLinks()
+    get_poster_links.save_links()
     # Create instance of GetNumbers
     get_numbers = ExtractData()
     # Start the phone number scraper
