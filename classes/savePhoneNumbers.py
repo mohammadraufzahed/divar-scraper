7d0bd61e0e096fe5ea6007c7674b792fc93b208a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-07-19 13:18:23 +0430

    Update the saveNumbers

--- classes/savePhoneNumbers.py
+++ classes/savePhoneNumbers.py
@@ -2,10 +2,13 @@
 Select the unique and valid phone numbers and save them in the text file
 """
 import psycopg2
-import config
+from persiantools.jdatetime import JalaliDate
+from datetime import datetime
+import random
 
 
 def savePhoneNumbers():
+    import config
     conn = psycopg2.connect(
         host=config.DB_HOST,
         user=config.DB_USER,
@@ -15,17 +18,31 @@ def savePhoneNumbers():
     curs = conn.cursor()
 
     curs.execute(
-        "SELECT DISTINCT phone FROM information WHERE phone ~ '^\d+$'")
+        """SELECT DISTINCT phone 
+        FROM information
+        WHERE phone ~ '^\d+$'
+        """)
     phoneNumbresDB = curs.fetchall()
     phoneNumbres = list()
     for i in phoneNumbresDB:
         phoneNumbres.append(i[0])
     del phoneNumbresDB
     counter = 0
-    with open("numbers.txt", "w+", encoding="utf8") as numbersFile:
+    category_name = input("Category Name: ")
+    category_name = category_name.split(' ')
+    category_name = "-".join(category_name).lower()
+    today_time = JalaliDate.today()
+    today_time = f"{today_time.year}-{today_time.month}-{today_time.day}"
+    rand_num = random.randint(0, datetime.now().microsecond)
+    file_name = f"{today_time}-{category_name}-{rand_num}.txt"
+    del today_time
+    del category_name
+    del rand_num
+    with open(f"output//{file_name}", "w+", encoding="utf8") as numbersFile:
         numbersFile.flush()
         for number in phoneNumbres:
-            numbersFile.write(f"TelNumber({counter})\t{str(number)}\n")
+            numbersFile.write(
+                f"+98{str(number)},این یک پیام تستی است لطفا دقیق بررسی نمایید\n")
             counter += 1
         numbersFile.close()
         print(f"{counter} phone number saved in the numbers.txt")
